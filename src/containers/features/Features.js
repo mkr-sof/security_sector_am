d39f0a7653eb4b6f100c4a31c1d3916e27c14bde
mkr-sof <mkrtchyan_sofia@mail.ru>
2023-11-20 13:21:58 +0400

    create new src

---
+++ src/containers/features/Features.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './featutes.css';
+
+const Features = () => {
+    return (
+        <div>
+            Features
+        </div>
+    );
+};
+
+export default Features;
