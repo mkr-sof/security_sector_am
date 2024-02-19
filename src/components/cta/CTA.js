1392045a95ea90918bebb67944c76ce5a2ac25de
mkr-sof <mkrtchyan_sofia@mail.ru>
2024-02-20 03:11:35 +0400

    CTA creating

--- src/components/cta/CTA.js
+++ src/components/cta/CTA.js
@@ -3,8 +3,15 @@ import './cta.css';
 
 const Cta = () => {
     return (
-        <div>
-            Cta
+        <div className="gpt3__cta">
+            <div className="gpt3__cta-content">
+                <p>Request</p>
+                <h3>Fill the Form</h3>
+            </div>
+            <div className="gpt3__cta-btn">
+                <button type="button">Get Started</button>
+            </div>
+
         </div>
     );
 };
